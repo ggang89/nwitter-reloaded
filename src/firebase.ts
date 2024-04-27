4d71c23195bf49ca0e6d820713b606323549f01a
ggang89 <ggang89@gmail.com>
2024-04-27 17:13:29 +0900

    firebase 초기설정=>계정에서 웹만들고 npm install firebase하고 firebase.ts만들어서 홈페이지에 있는 코두 붙여 넣기

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7kuOGskn7GUHnwes4bph10rws0xS08KA",
+  authDomain: "nwitter-reloaded-1f483.firebaseapp.com",
+  projectId: "nwitter-reloaded-1f483",
+  storageBucket: "nwitter-reloaded-1f483.appspot.com",
+  messagingSenderId: "695412618678",
+  appId: "1:695412618678:web:a3af891c69d38f81877d17",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
